5b6de65a99a5e1836c53573228340c381c1080bb
Bảo Trần <baotnn.22git@vku.udn.vn>
2025-09-14 21:36:53 +0700

    add gun's animation and crosshair

--- Assets/Scripts/Weapons/HandgunFire.cs
+++ Assets/Scripts/Weapons/HandgunFire.cs
@@ -1,15 +1,35 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     [SerializeField] AudioSource gunFire;
+    [SerializeField] GameObject handgun;
+    [SerializeField] bool canFire = true;
+    [SerializeField] GameObject extraCross;
     // Update is called once per frame
     void Update()
     {
         if (Mouse.current.leftButton.isPressed)
         {
-            gunFire.Play();
+            if (canFire == true)
+            {
+                canFire = false;
+                StartCoroutine(FiringGun());
+            }
         }
+    }
+
+    IEnumerator FiringGun()
+    {
+        gunFire.Play();
+        extraCross.SetActive(true);
+        handgun.GetComponent<Animator>().Play("HandgunFire");
+        yield return new WaitForSeconds(0.5f);
+        handgun.GetComponent<Animator>().Play("New State");
+        extraCross.SetActive(false);
+        yield return new WaitForSeconds(0.1f);
+        canFire = true;
     }
 }
